762e0238033ee812074b5347f3d1796906d4654a
Richard Dymond <rjdymond@gmail.com>
2018-05-02 09:53:23 -0300

    Update OUTPUT in write-tests.py for SkoolKit 7.0

--- utils/write-tests.py
+++ utils/write-tests.py
@@ -16,76 +16,75 @@ SKOOL = 'sd.skool'
 
 SNAPSHOT = 'build/skool_daze.z80'
 
-OUTPUT = """Using skool file: sd.skool
-Using ref files: sd.ref, bugs.ref, changelog.ref, data.ref, facts.ref, glossary.ref, graphics.ref, pages.ref, pokes.ref
+OUTPUT = """Using ref files: sd.ref, bugs.ref, changelog.ref, data.ref, facts.ref, glossary.ref, graphics.ref, pages.ref, pokes.ref
 Parsing sd.skool
-Creating directory {odir}/skool_daze
-Copying {SKOOLKIT_HOME}/skoolkit/resources/skoolkit.css to {odir}/skool_daze/skoolkit.css
-Copying sd.css to {odir}/skool_daze/sd.css
-  Writing disassembly files in skool_daze/asm
-  Writing skool_daze/maps/all.html
-  Writing skool_daze/maps/routines.html
-  Writing skool_daze/maps/data.html
-  Writing skool_daze/maps/messages.html
-  Writing skool_daze/buffers/gbuffer.html
-  Writing skool_daze/reference/bugs.html
-  Writing skool_daze/reference/changelog.html
-  Writing skool_daze/reference/facts.html
-  Writing skool_daze/reference/glossary.html
-  Writing skool_daze/graphics/glitches.html
-  Writing skool_daze/reference/pokes.html
-  Writing skool_daze/graphics/graphics.html
-  Writing skool_daze/graphics/playarea.html
-  Copying tiles.js to {odir}/skool_daze/tiles.js
-  Writing skool_daze/graphics/patiles/patiles.html
-  Writing skool_daze/graphics/asstart.html
-  Writing skool_daze/graphics/as.html
-  Writing skool_daze/graphics/astiles/astiles.html
-  Writing skool_daze/buffers/cbuffer.html
-  Writing skool_daze/lessons/timetables.html
-  Writing skool_daze/lessons/index.html
-  Writing skool_daze/lessons/#N224.html
-  Writing skool_daze/lessons/#N225.html
-  Writing skool_daze/lessons/#N226.html
-  Writing skool_daze/lessons/#N227.html
-  Writing skool_daze/lessons/#N228.html
-  Writing skool_daze/lessons/#N229.html
-  Writing skool_daze/lessons/#N230.html
-  Writing skool_daze/lessons/#N231.html
-  Writing skool_daze/lessons/#N232.html
-  Writing skool_daze/lessons/#N233.html
-  Writing skool_daze/lessons/#N234.html
-  Writing skool_daze/lessons/#N235.html
-  Writing skool_daze/lessons/#N236.html
-  Writing skool_daze/lessons/#N237.html
-  Writing skool_daze/lessons/#N238.html
-  Writing skool_daze/lessons/#N239.html
-  Writing skool_daze/lessons/#N240.html
-  Writing skool_daze/lessons/#N241.html
-  Writing skool_daze/lessons/#N242.html
-  Writing skool_daze/lessons/#N243.html
-  Writing skool_daze/lessons/#N244.html
-  Writing skool_daze/lessons/#N245.html
-  Writing skool_daze/lessons/#N246.html
-  Writing skool_daze/lessons/#N247.html
-  Writing skool_daze/lessons/#N248.html
-  Writing skool_daze/lessons/#N249.html
-  Writing skool_daze/lessons/#N250.html
-  Writing skool_daze/lessons/#N251.html
-  Writing skool_daze/lessons/#N252.html
-  Writing skool_daze/lessons/#N253.html
-  Writing skool_daze/lessons/#N254.html
-  Writing skool_daze/lessons/#N255.html
-  Writing skool_daze/tables/keys.html
-  Parsing load.skool
-    Writing skool_daze/load/load.html
-    Writing disassembly files in skool_daze/load
-  Parsing save.skool
-    Writing skool_daze/save/save.html
-    Writing disassembly files in skool_daze/save
-  Parsing start.skool
-    Writing skool_daze/start/start.html
-    Writing disassembly files in skool_daze/start
-  Writing skool_daze/index.html"""
+Output directory: {odir}/skool_daze
+Copying {SKOOLKIT_HOME}/skoolkit/resources/skoolkit.css to skoolkit.css
+Copying sd.css to sd.css
+Writing disassembly files in asm
+Writing maps/all.html
+Writing maps/routines.html
+Writing maps/data.html
+Writing maps/messages.html
+Writing buffers/gbuffer.html
+Writing reference/bugs.html
+Writing reference/changelog.html
+Writing reference/facts.html
+Writing reference/glossary.html
+Writing graphics/glitches.html
+Writing reference/pokes.html
+Writing graphics/graphics.html
+Writing graphics/playarea.html
+Copying tiles.js to tiles.js
+Writing graphics/patiles/patiles.html
+Writing graphics/asstart.html
+Writing graphics/as.html
+Writing graphics/astiles/astiles.html
+Writing buffers/cbuffer.html
+Writing lessons/timetables.html
+Writing lessons/index.html
+Writing lessons/#N224.html
+Writing lessons/#N225.html
+Writing lessons/#N226.html
+Writing lessons/#N227.html
+Writing lessons/#N228.html
+Writing lessons/#N229.html
+Writing lessons/#N230.html
+Writing lessons/#N231.html
+Writing lessons/#N232.html
+Writing lessons/#N233.html
+Writing lessons/#N234.html
+Writing lessons/#N235.html
+Writing lessons/#N236.html
+Writing lessons/#N237.html
+Writing lessons/#N238.html
+Writing lessons/#N239.html
+Writing lessons/#N240.html
+Writing lessons/#N241.html
+Writing lessons/#N242.html
+Writing lessons/#N243.html
+Writing lessons/#N244.html
+Writing lessons/#N245.html
+Writing lessons/#N246.html
+Writing lessons/#N247.html
+Writing lessons/#N248.html
+Writing lessons/#N249.html
+Writing lessons/#N250.html
+Writing lessons/#N251.html
+Writing lessons/#N252.html
+Writing lessons/#N253.html
+Writing lessons/#N254.html
+Writing lessons/#N255.html
+Writing tables/keys.html
+Parsing load.skool
+Writing load/load.html
+Writing disassembly files in load
+Parsing save.skool
+Writing save/save.html
+Writing disassembly files in save
+Parsing start.skool
+Writing start/start.html
+Writing disassembly files in start
+Writing index.html"""
 
 write_tests(SKOOL, SNAPSHOT, OUTPUT)
